6681ce4d2968f43efe5570479477acfd8f46799d
aggale <annagrgale@gmail.com>
2021-04-23 10:49:53 -0400

    Built out basic bio; few css fixes

--- src/App.js
+++ src/App.js
@@ -5,23 +5,16 @@ import Projects from "./Projects/Projects";
 import data from "./data.json";
 import Blog from "./Blog/Blog";
 import BlogPost from "./Blog/BlogPost";
+import Main from "./Bio/Main";
 
 function App() {
   return (
     <div>
       <Header />
+      <Main />
       {/*<Projects projects={data.projects} />
-      <Blog blogPosts={data["blog"]} />*/}
-      <BlogPost
-        blogPost={{
-          title: "Images are the worst ever",
-          summary:
-            "I had this problem getting my React images to appear when I have a dynamic list",
-          text:
-            "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
-          posted: "August 5, 2019",
-        }}
-      />
+      <Blog blogPosts={data["blog"]} />
+      <BlogPost blogPost={data["blog"]["1"]} />*/}
     </div>
   );
 }
--- src/Bio/Main.js
+++ src/Bio/Main.js
@@ -4,25 +4,53 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 import profilePic from "../assets/images/cartoon-me-cutout.png";
-import photo from "../assets/images/karlie.jpg";
+import jsLogo from "../assets/images/javascript.png";
+import reactLogo from "../assets/images/react.png";
+import csLogo from "../assets/images/csharp.png";
+import cppLogo from "../assets/images/cplus.png";
+
+import "./Bio.css";
 
 const Main = () => {
   return (
     <div id="main">
-      <img src={profilePic} alt="Me" className="profile-pic" />
       <header className="mt-5 mb-5">
-        <h1>I'm Anna and I'm unhinged so I code</h1>
+        <img src={profilePic} alt="Me" className="profile-pic" />
+        <h1>I'm Anna, and I'm a software developer.</h1>
       </header>
-      <Container className="side-by-side">
-        <Row>
+      <Container className="about-me">
+        <Row className="mb-5">
           <Col>
-            <p>
-              There's so much to know about me like I eat avocados and like to
-              smell candles.
-            </p>
+            <img src={jsLogo} alt="JavaScript" className="grid-pic" />
           </Col>
           <Col>
-            <img src={photo} alt="Karlie" className="profile-pic" />
+            <img src={reactLogo} alt="React" className="grid-pic" />
+          </Col>
+          <Col>
+            <img src={csLogo} alt="JavaScript" className="grid-pic" />
+          </Col>
+          <Col>
+            <img src={cppLogo} alt="JavaScript" className="grid-pic" />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p className="grid-description">
+              {
+                "I'm a full stack web developer with front-end experience using JavaScript, React, Angular, TypeScript, and HTML/CSS -- and back-end experience with C#, C/C++, and SQL server."
+              }
+            </p>
+
+            <p className="grid-description">
+              {
+                "I also have professional experience with user-facing technical writing, automated system testing, and customer support, and graduate-level academic experience with NLP and machine learning."
+              }
+            </p>
+            <p className="grid-description emphasized">
+              {
+                "I am passionate about making software that is easy to use and makes a difference in people's lives."
+              }
+            </p>
           </Col>
         </Row>
       </Container>
