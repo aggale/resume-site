0f578dd68a14f0902e0d380b7b10b40255559609
aggale <annagrgale@gmail.com>
2021-04-27 10:29:38 -0400

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Projects from "./Projects/Projects";
 import data from "./data.json";
 import Blog from "./Blog/Blog";
@@ -9,13 +10,25 @@ import Main from "./Bio/Main";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Main />
-      {/*<Projects projects={data.projects} />
-      <Blog blogPosts={data["blog"]} />
-      <BlogPost blogPost={data["blog"]["1"]} />*/}
-    </div>
+    <Router>
+      <div>
+        <Header />
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/projects">
+            <Projects projects={data.projects} />
+          </Route>
+          <Route path="/blog">
+            <Blog blogPosts={data["blog"]} />
+          </Route>
+          <Route path="/">
+            <Main />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
 const NavBar = () => {
   return (
-    <div className="navbar">
-      <Button className="nav-item" size="xxl">
-        Bio
-      </Button>
-      <Button className="nav-item" size="xxl">
-        Projects
-      </Button>
-      <Button className="nav-item" size="xxl">
-        Blog
-      </Button>
-    </div>
+    <nav>
+      <ul className="nav-bar">
+        <li className="nav-item">
+          <Link to="/">Bio</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/blog">Blog</Link>
+        </li>
+      </ul>
+    </nav>
+    // <nav className="navbar">
+    //   <Button className="nav-item" size="xxl">
+    //     Bio
+    //   </Button>
+    //   <Button className="nav-item" size="xxl">
+    //     Projects
+    //   </Button>
+    //   <Button className="nav-item" size="xxl">
+    //     Blog
+    //   </Button>
+    //</nav>
   );
 };
 
