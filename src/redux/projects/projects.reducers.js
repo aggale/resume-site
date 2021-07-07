122cc21b13589bc97751e3b9fc23d8d13c9dc6c4
aggale <annagrgale@gmail.com>
2021-07-07 14:48:48 -0400

    Removed data from init in reducer

--- src/redux/projects/projects.reducers.js
+++ src/redux/projects/projects.reducers.js
@@ -1,25 +1,6 @@
 import {ProjectsActionTypes} from './projects.types'
 
-const INITIAL_STATE = {projectList: {
-    "resume-site": {
-      "title": "Resume Websit",
-      "image": "resume-website-thumbnail.jpg",
-      "technologies": "React, JavaScript, CSS, Bootstrap",
-      "description": "Built a Resume website featuring project information and a blog using React.js and React-bootstrap. Hosted using AWS.",
-      "url": "http://annaggale.com",
-      "github": "https://github.com/aggale/resume-site",
-      "order": 1
-    },
-    "old-blog": {
-      "title": "My Old Blog",
-      "image": "old-blog-thumbnail.jpg",
-      "technologies": "NLP, JavaScript, Bootstrap, DynamoDB, Arduino",
-      "description": "This was a blog where I wrote about some of the projects I was doing in 2017-2018. I talk about building websites, some of my graduate NLP work, and playing with Arduino.",
-      "url": "https://annaggale.wordpress.com/",
-      "github": "",
-      "order": 2
-    }
-}};
+const INITIAL_STATE = {projectList: {}};
 
 const projectsReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
